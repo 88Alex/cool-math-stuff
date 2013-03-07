002016eada29c5cb3ca03c4c17bc545db4b5df15
Alex <epicpiano8@gmail.com>
2013-03-07 16:42:17 -0500

    Create Primes.py

---
+++ Primes.py
@@ -0,0 +1,16 @@
+import time
+def isPrime(a):
+    i=2
+    while i<a:
+        if a%i==0:
+            return False
+        i=i+1
+    return True
+number=int(input("Maximum number to check? "))
+q=2
+while q<=number:
+    if isPrime(q):
+        print(q," is PRIME!!!")
+    else:
+        print(q," is not prime.")
+    time.sleep(0.5)
