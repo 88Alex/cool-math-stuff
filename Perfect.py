82112985c6b6629b4e2dc70f244b36d0114c097d
Alex <epicpiano8@gmail.com>
2013-03-07 15:47:56 -0500

    Create Perfect.py

---
+++ Perfect.py
@@ -0,0 +1,20 @@
+def sumOfFactors(a):
+    factors=[]
+    i=1
+    while i<a:
+        if(a%i==0):
+            factors.append()
+    sum=0
+    for q in factors:
+        sum = sum + q
+    return sum
+number = int(raw_input("Maximum number to check? "))
+i=2
+while i<=number:
+    factorSum = sumOfFactors(i)
+    if factorSum < i:
+        print(i," is deficient.")
+    elif factorSum > i:
+        print(i," is abundant!")
+    elif factorSum == i:
+        print(i," is PERFECT!!!")
